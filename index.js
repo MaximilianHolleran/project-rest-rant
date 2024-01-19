69cae65e58e2992f7664d46479ed1993960308cd
MaximilianHolleran <mhholleran@gmail.com>
2024-01-19 15:23:16 -0500

    added 404 page route

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+require('dotenv').config() 
 const express = require('express')
 const app = express()
+
 app.get('/', function(req,res){
     res.send('hellow world')
 })
-app.listen(3000)
+
+app.get('*', function(req,res){
+    res.status(404).send('<h1>404 Page GOT EM</h1>')
+})
+
+app.listen(process.env.PORT)
 
