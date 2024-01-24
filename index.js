cf63446a5208ac073d0c9b244473e633773224ff
MaximilianHolleran <mhholleran@gmail.com>
2024-01-23 20:49:31 -0500

    added css to rest rant

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-require('dotenv').config() 
+// Modules and Globals
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-app.get('/', function(req,res){
+app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('*', function(req,res){
+app.get('*', (req, res) => {
     res.render('error404')
 })
 
-// GET /places
-app.get('/', (req, res) => {
-    let places = []
-    res.render('places/index')
-  })
-  
-
+// Listen for Connections
 app.listen(process.env.PORT)
--- views/home.jsx
+++ views/home.jsx
@@ -1,14 +1,17 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
+function Home() {
     return (
       <Def>
           <main>
               <h1>HOME</h1>
+                <a href="/places">
+                <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Def>
     )
   }
 
-module.exports = home
+module.exports = Home
