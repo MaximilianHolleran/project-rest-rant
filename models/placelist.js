45356a932c8000962baff74c63b2c38c1db683a5
MaximilianHolleran <mhholleran@gmail.com>
2024-01-30 21:06:34 -0500

    mongoose is great they said

--- models/placelist.js
+++ models/placelist.js
@@ -1,19 +1,32 @@
-module.exports =  [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/jerome-jome-unsplash.jpg',
-    credit: "https://unsplash.com/@jomemui?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-    creditName: 'Jerome Jome'
+// BEFORE MONGOOSE module.exports =  [{
+//     name: 'H-Thai-ML',
+//     city: 'Seattle',
+//     state: 'WA',
+//     cuisines: 'Thai, Pan-Asian',
+//     pic: '/images/jerome-jome-unsplash.jpg',
+//     credit: "https://unsplash.com/@jomemui?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+//     creditName: 'Jerome Jome'
     
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/reba-spike-unsplash.jpg',
-    credit: "https://unsplash.com/@rebaspike?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
-    creditName: 'Reba Spike'
+//   }, {
+//     name: 'Coding Cat Cafe',
+//     city: 'Phoenix',
+//     state: 'AZ',
+//     cuisines: 'Coffee, Bakery',
+//     pic: '/images/reba-spike-unsplash.jpg',
+//     credit: "https://unsplash.com/@rebaspike?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash",
+//     creditName: 'Reba Spike'
    
-  }]
+//   }]
+
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
