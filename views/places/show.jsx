8af277d1ddf35c88c1185b91b1914b8f6c7ed47a
MaximilianHolleran <mhholleran@gmail.com>
2024-02-06 22:02:14 -0500

    delete places button added

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,20 +7,38 @@ function show (data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for(let i = 0; i < averageRating; i++) {
+            stars += '⭐'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
-          return (
-            <div className="border">
-              <h2 className="rant">{c.rant ? 'Rant!' : 'Rave!'}</h2>
-              <h4>{c.content}</h4>
-              <h3>
-                <strong>- {c.author}</strong>
-              </h3>
-              <h4>Rating: {c.stars}</h4>
-            </div>
-          )
+            return(
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 🤩'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
         })
-      }
+    }
     return (
         <Def>
             <main>
@@ -30,10 +48,8 @@ function show (data) {
                 <h3>{data.place.showEstablished}</h3>
                 <h3> Serving{ data.place.cuisines }</h3> 
                 <div>
-                <h2>Rating</h2>
-                <p>Not Rated</p>
-                <h2>Comments</h2>
-                {comments}
+                <h2>{rating}</h2>
+                <h3>{comments}</h3>
                 <form method="POST" action={`/places/${data.place.id}/comment`} className="p-3">
                     <div className="form-group">
                         <label htmlFor="author">Author</label>
@@ -54,12 +70,12 @@ function show (data) {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
                 </div> 
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
                     Edit
                 </a>  
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
-                        Delete
+                        Delete Place
                     </button>
                 </form>     
 
@@ -69,27 +85,3 @@ function show (data) {
 }
 
 module.exports = show
-
-/* <Def>
-            <main>
-                <h1>{ data.place.name }</h1>
-                <img src={ data.place.pic} alt={data.place.credit}/>
-                <h4>{ data.place.city }, {data.place.state}</h4>
-                <h3>{ data.place.cuisines }</h3> 
-                <div>
-                <h2>Rating</h2>
-                <p>Not Rated</p>
-                <h2>Comments</h2>
-                <p>No comments yet!</p>
-                </div> 
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                    Edit
-                </a>  
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>     
-
-            </main>
-        </Def> */
